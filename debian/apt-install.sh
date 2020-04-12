caab3286e3c5e1b5534d779adf177b3265ecd16d
Jack <jackson.virgo1@gmail.com>
2020-04-12 12:42:30 +0930

    Zsh and things

--- debian/apt-install.sh
+++ debian/apt-install.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 add-apt-repository ppa:neovim-ppa/unstable
 add-apt-repository ppa:jonathonf/vim
 
 apt install neovim vim
 
-apt install git htop curl lsd
+apt install git htop curl lsd tmux tilix
